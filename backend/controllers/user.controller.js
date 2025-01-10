fa4217c82577001177d894c2112adb29ae7b1448
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-01-10 20:51:31 +0530

    Redis setup

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import userModel from '../models/user.model.js';
 import * as userService from '../services/user.service.js';
 import {validationResult} from 'express-validator';
+import redisClient from '../services/redis.service.js';
 
 export const createUserController = async (req, res) => {
     const errors = validationResult(req);
@@ -52,3 +53,4 @@ export const profileController = async (req, res) => {
         user: req.user
     });
 }
+
