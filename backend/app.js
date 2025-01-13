e551725ea2240dc4ee28a08d07b941a7389b648f
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-01-13 11:29:26 +0530

    add axios for API requests and implement login/register functionality with form handling

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,16 @@ import morgan from 'morgan';
 import connect from './db/db.js';
 import userRoutes from './routes/user.routes.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 connect();
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -12,6 +12,7 @@ export const createUserController = async (req, res) => {
     try {
         const user = await userService.createUser(req.body);
         const token = await user.generateJWT();
+        delete user._doc.password;
         res.status(201).json({ user, token }); 
     } catch (error) {
         res.status(400).send(error.message);
@@ -40,6 +41,7 @@ export const loginUserController = async (req, res) => {
             })
         }
         const token = await user.generateJWT();
+        delete user._doc.password;
         res.status(200).json({ user, token });
     }
     catch (err) {
