1d731f8339e205d01d78f27e202deae431bf463e
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-01-26 12:18:35 +0530

    enhance Project screen layout with sidebar and header components

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -5,7 +5,14 @@ const Project = ({navigate}) => {
     const location = useLocation()
     console.log(location.state)
   return (
-    <div>Project</div>
+    <main className='h-screen w-screen flex'>
+        <section className='left h-full min-w-80 bg-red-300'>
+            <header className='flex justify-end p-3 px-4 w-full bg-slate-300'>
+            <button className='p-2'><i className="ri-group-fill"></i></button>
+            </header>
+        </section> 
+
+    </main>
   )
 }
 
