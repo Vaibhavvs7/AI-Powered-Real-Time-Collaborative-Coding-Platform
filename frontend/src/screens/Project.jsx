e62abe67644378d041ecf8c29cdbbbe03c207868
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-01-27 20:22:37 +0530

    add side panel for collaborators in Project screen with toggle functionality

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 const Project = ({navigate}) => {
+
     const location = useLocation()
+    const [isSidePanelOpen, setIsSidePanelOpen] = useState(false)
+
     console.log(location.state)
   return (
     <main className='h-screen w-screen flex'>
-        <section className='left relative flex flex-col h-full min-w-72 bg-slate-300'>
+        <section className='left relative flex flex-col h-full min-w-96 bg-slate-300'>
             <header className='flex justify-end p-2 px-4 w-full bg-slate-100'>
-            <button className='p-2'><i className="ri-group-fill"></i></button>
+            <button onClick={() => setIsSidePanelOpen(!isSidePanelOpen)} className='p-2'><i className="ri-group-fill"></i></button>
             </header>
             <div className="conversation-area flex-grow flex flex-col">
                 <div className="message-box flex-grow flex flex-col gap-1 p-1">
@@ -22,11 +25,31 @@ const Project = ({navigate}) => {
                     </div>
                 </div>
                 <div className="inputField w-full flex">
-                    <input className='p-2 px-4 border-none outline-none' type='text' placeholder='Type a message'/>
-                    <button className='flex-grow px-3'><i className="ri-send-plane-fill"></i></button>
+                    <input className='p-2 px-4 border-none outline-none flex-grow' type='text' placeholder='Type a message'/>
+                    <button className='px-5 bg-slate-900 text-white'><i className="ri-send-plane-fill"></i></button>
                 </div>
 
-                <div className="sidePanel w-36 h-60 bg-red-600 absolute left-[-100%] top-0"></div>
+                <div className={`sidePanel w-full h-full flex flex-col gap-2 bg-slate-50 absolute transition-all ${isSidePanelOpen ? 'translate-x-0' : '-translate-x-full'} top-0`}>
+                <header className='flex justify-between items-center px-4 p-2 bg-slate-200'>
+
+                    <h1
+                        className='font-semibold text-lg'
+                    >Collaborators</h1>
+
+                <button onClick={() => setIsSidePanelOpen(!isSidePanelOpen)} className='p-2'>
+                    <i className="ri-close-fill"></i>
+                </button>
+                </header>
+
+                <div className="users flex flex-col gap-2">
+                    <div className="user cursor-pointer hover:bg-slate-100 p-2 flex gap-2 items-center">
+                        <div className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
+                            <i className="ri-user-fill absolute"></i>
+                        </div>
+                        <h1 className='font-semibold text-lg'>username</h1>
+                    </div>
+                </div>
+                </div>
             </div>
         </section> 
 
