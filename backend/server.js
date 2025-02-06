09925b545f5778a1792f356f90fd5c7347d9a6c6
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-02-06 15:22:09 +0530

    add socket.io and engine.io modules with type definitions and wrapper files

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,20 @@ import 'dotenv/config';
 
 import http from 'http';
 import app from './app.js';
+import {Server} from 'socket.io'
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const io = new Server(server);
+
+
+io.on('connection', socket => {
+
+    console.log('a user connected');
+  client.on('event', data => { /* … */ });
+  client.on('disconnect', () => { /* … */ });
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
