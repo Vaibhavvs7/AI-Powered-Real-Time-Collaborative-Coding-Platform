0b0b4ba81b34d4758e7fa5eaf5025cf760131c59
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-02-06 15:33:07 +0530

    add JWT authentication middleware for socket connections

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,37 @@ import 'dotenv/config';
 import http from 'http';
 import app from './app.js';
 import {Server} from 'socket.io'
+import jwt from 'jsonwebtoken'
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 const io = new Server(server);
 
+io.use((socket, next) => {
+    try{
+        const token = socket.handshake.auth?.token || socket.handshake.headers.authorization?.split(' ')[1];
+        if(!token){
+            return next(new Error('Authentication Error'))
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if(!decoded){
+            return next(new Error('Authentication Error'))
+        }
+        socket.user = decoded;
+
+        next();
+
+    }catch(err){
+        next(err)
+    }
+})
+
 
 io.on('connection', socket => {
-
     console.log('a user connected');
-  client.on('event', data => { /* … */ });
-  client.on('disconnect', () => { /* … */ });
+    client.on('event', data => { /* … */ });
+    client.on('disconnect', () => { /* … */ });
 });
 
 server.listen(port, () => {
